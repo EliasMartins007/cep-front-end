75c6011bad8f23b4e7c49970fdeed99adf72b69d
elias <eliasfaculdade@yahoo.com.br>
2021-03-30 18:28:08 -0300

    front end-cep

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData); //data = dados
+    console.log(data);
+
+    fetch('http://localhost:3003/?tracking' + data.tracking)
+      .then((response) => response.json())
+      .then(console.log) //se der certo !
+      .catch(console.error); //caso ocorra erro
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <h1>BUSCAR CEP:</h1>
+      <form onSubmit={submitHandler}>
+        <div className="form-group">
+          <input type="text" name="tracking" className="form-control" />
+        </div>
+        <button type="submit" name="TrackCEP" className="btn btn-primary">
+          Buscar
+        </button>
+      </form>
     </div>
   );
 }
